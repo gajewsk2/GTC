fe4341ab335b5cc0314b88895c089aff5ed954b5
Matt Lodes <lodes2@illinois.edu>
2013-07-30 15:16:32 -0500

    added the BaseAdapter and ListViewAdapter

---
+++ GuitarTensionCalculator/src/com/example/guitartensioncalculator/CustomGuitarAdapter.java
@@ -0,0 +1,72 @@
+package com.example.guitartensioncalculator;
+
+import guitarTensionCalc.GuitarTensionCalc;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+
+public class CustomGuitarAdapter extends BaseAdapter{
+
+	//Array that holds the data for the strings
+	private ArrayList<GuitarTensionCalc> _gString;
+	
+	Context _context;
+	
+	//Adapter that binds the user data to the listView
+	CustomGuitarAdapter (ArrayList<GuitarTensionCalc> gString, Context context){
+		_gString = gString;
+		_context = context;
+	}
+	
+	
+	//Number elements in the ArrayList<GuitarTensionCalc> which is the number of strings
+	@Override
+	public int getCount() {
+		return _gString.size();
+	}
+
+	//Returns the element that is clicked by the user which is the the string they clicked on the listView
+	@Override
+	public Object getItem(int position){
+		return _gString.get(position);
+	}
+
+	//Returns the index of of ArrayList<GuitarTensionCalc> that the user clicked on the ListView, only the index and not the string Object
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	
+	
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View v = convertView;
+        if (v == null)
+        {
+           LayoutInflater layout_inflater = (LayoutInflater)_context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+           v = layout_inflater.inflate(R.layout.string_layout, null);
+        }
+        
+        //Gets the address of the Buttons on the ListView Row
+		Button frequencyButton = (Button) v.findViewById(R.id.buttonFrequency);
+		Button gaugeButton = (Button) v.findViewById(R.id.buttonGauge);
+		Button typeButton = (Button) v.findViewById(R.id.buttonType);
+
+		//Gets the Values from the gString to set on the ListView row
+		GuitarTensionCalc guitarString = _gString.get(position);
+		
+		//Sets the Correct Values to the Buttons  
+		frequencyButton.setText("Frequency");
+		gaugeButton.setText("Gauge");
+		typeButton.setText("Type");
+		
+		return v;
+	}
+}
---
+++ GuitarTensionCalculator/src/com/example/guitartensioncalculator/gStringListItem.java
@@ -0,0 +1,13 @@
+package com.example.guitartensioncalculator;
+
+import android.content.Context;
+import android.widget.ListView;
+
+public class gStringListItem extends ListView{
+
+	public gStringListItem(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+}
