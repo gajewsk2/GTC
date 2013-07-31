f66d57290c7bdcbb985c246cdb2e54d2dc832d1f
Matt Lodes <lodes2@illinois.edu>
2013-07-31 00:04:51 -0500

    Adding Strings Now mostly works but editing does not

--- GuitarTensionCalculator/src/com/example/guitartensioncalculator/MainActivity.java
+++ GuitarTensionCalculator/src/com/example/guitartensioncalculator/MainActivity.java
@@ -18,13 +18,16 @@ import android.widget.Button;
 import android.widget.ListView;
 
 public class MainActivity extends Activity {
+	final ArrayList<GuitarTensionCalc> stringArray = new ArrayList<GuitarTensionCalc>();
+	final CustomGuitarAdapter adapter = new CustomGuitarAdapter(stringArray,this);
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		//setContentView(R.layout.activity_main);
 		setContentView(R.layout.activity_main);
-		
+		final Intent intent = new Intent(MainActivity.this, StringInformationActivity.class);
+
 		
 		// Getting listview from xml
 		ListView stringList = (ListView) findViewById(R.id.GuitarStringList);
@@ -35,6 +38,7 @@ public class MainActivity extends Activity {
 		stringList.addFooterView(addStringButton);
 		
 		addStringButton.setOnClickListener(new Button.OnClickListener() { public void onClick (View v){
+			startActivityForResult(intent, 1);
 			Log.i("button", "Click");
 			
 		}});
@@ -51,21 +55,17 @@ public class MainActivity extends Activity {
 		
 		
 		
-		final Intent intent = new Intent(MainActivity.this, StringInformationActivity.class);
-		//startActivity(intent);
 		
 		
 		LayoutInflater inflater = getLayoutInflater();
 		ViewGroup header = (ViewGroup)inflater.inflate(R.layout.listview_header, stringList, false);
 		stringList.addHeaderView(header, null, false);
 		
-		final ArrayList<GuitarTensionCalc> stringArray = new ArrayList<GuitarTensionCalc>();
 		GuitarTensionCalc temp = new GuitarTensionCalc();
 		temp.freq = 1;
 		temp.scaleLength = 2;
 		temp.unitWeight = 3;
 		stringArray.add(temp);
-		final CustomGuitarAdapter adapter = new CustomGuitarAdapter(stringArray,this);
 		stringList.setAdapter(adapter);
 		
 		
@@ -96,6 +96,39 @@ public class MainActivity extends Activity {
 	        }
 	    });
 	    }
+	
+	
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    // TODO Auto-generated method stub
+	    super.onActivityResult(requestCode, resultCode, data);
+	    Intent intent = new Intent();
+	    if (requestCode == 1){
+    		Log.i("Activity", "Returned From Activity:1");
+	    	if(data == null){
+	    		Log.i("Add String", "Data bundle was empty");
+	    		return;
+	    	}
+	    int octave = data.getIntExtra("octave", -1);
+	    int note = data.getIntExtra("note", -1);
+	    Double guage = data.getDoubleExtra("guage", -1);
+
+	    Log.i("Caller", Integer.toString(octave));
+	    Log.i("Caller", Integer.toString(note));
+	    Log.i("Caller", Double.toString(guage));
+	    
+		GuitarTensionCalc temp = new GuitarTensionCalc();
+		temp.freq = octave;
+		temp.scaleLength = note;
+		temp.unitWeight = guage;
+		stringArray.add(temp);
+		adapter.notifyDataSetChanged();
+
+	    
+	    	
+	    }
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
--- GuitarTensionCalculator/src/com/example/guitartensioncalculator/StringInformationActivity.java
+++ GuitarTensionCalculator/src/com/example/guitartensioncalculator/StringInformationActivity.java
@@ -1,8 +1,13 @@
 package com.example.guitartensioncalculator;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.NumberPicker;
 
 public class StringInformationActivity extends Activity {
@@ -12,10 +17,10 @@ public class StringInformationActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.test_layout);
 		
-		NumberPicker np1 = (NumberPicker) findViewById(R.id.numberPicker1);
+		final NumberPicker np1 = (NumberPicker) findViewById(R.id.numberPicker1);
 		np1.setMaxValue(10);
 		
-		NumberPicker np2 = (NumberPicker) findViewById(R.id.numberPicker2);
+		final NumberPicker np2 = (NumberPicker) findViewById(R.id.numberPicker2);
 		String[] notes = new String[15];
 		notes[0] = "A";
 		notes[1] = "B";
@@ -36,7 +41,29 @@ public class StringInformationActivity extends Activity {
 		np2.setMinValue(0);
 		np2.setWrapSelectorWheel(true);
 		np2.setDisplayedValues(notes);
-	}
+		
+		
+		Button submit = (Button)findViewById(R.id.buttonSubmit);
+		submit.setOnClickListener(new Button.OnClickListener() { public void onClick (View v){
+			EditText userGuage = (EditText)findViewById(R.id.editTextGauge);
+			Log.i("submit", "submit");
+			Log.i("OctavePicker", Integer.toString(np1.getValue()));
+			Log.i("NotePicker", Integer.toString(np2.getValue()));
+			Log.i("Gauge", userGuage.getText().toString());
+			String guage = userGuage.getText().toString();
+			if(guage.matches("") || guage.matches(".")){
+				Log.i("Gauge", "null");
+			}else{
+
+			Intent returnToCaller = new Intent();
+		    returnToCaller.putExtra("octave", np1.getValue());
+		    returnToCaller.putExtra("note", np2.getValue());
+		    returnToCaller.putExtra("guage", Double.parseDouble(guage));
+
+		    setResult(1,returnToCaller);        
+		    finish();
+			}
+		}});	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
