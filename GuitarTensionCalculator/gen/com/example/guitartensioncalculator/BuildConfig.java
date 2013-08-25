ba0876d8b067d1f56771d3481e4afea58c0a86e9
Matt Lodes <lodes2@illinois.edu>
2013-08-25 14:03:36 -0500

    gen

---
+++ GuitarTensionCalculator/gen/com/example/guitartensioncalculator/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.guitartensioncalculator;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
